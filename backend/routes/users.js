ceff85012c9890d72adb077887dc7c6b58c71ab6
thong89x <thong89x@gmail.com>
2022-12-10 20:14:57 +0700

    use Middleware to verify jwt

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const usersController = require('../controllers/usersController')
-// const verifyJWT = require('../middleware/verifyJWT')
+const verifyJWT = require('../middleware/verifyJWT')
 
-// router.use(verifyJWT)
+router.use(verifyJWT)
 
 router.route('/')
 .get(usersController.getAllUsers)
