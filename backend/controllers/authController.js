127c607d778e6d5196a3cbb1d44204d93a3631cc
thong89x <thong89x@gmail.com>
2022-12-10 20:10:15 +0700

    set up login send toKen and logou remove token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,80 +7,80 @@ const asyncHandler = require('express-async-handler')
 // @route POST /auth
 // @access Public
 const login = asyncHandler(async (req, res) => {
-    // const { username, password } = req.body
+    const { username, password } = req.body
 
-    // if (!username || !password) {
-    //     return res.status(400).json({ message: 'All fields are required' })
-    // }
+    if (!username || !password) {
+        return res.status(400).json({ message: 'All fields are required' })
+    }
 
-    // const foundUser = await User.findOne({ username }).exec()
+    const foundUser = await User.findOne({ username }).exec()
 
-    // if (!foundUser || !foundUser.active) {
-    //     return res.status(401).json({ message: 'Unauthorized' })
-    // }
+    if (!foundUser || !foundUser.active) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
 
-    // const match = await bcrypt.compare(password, foundUser.password)
+    const match = await bcrypt.compare(password, foundUser.password)
 
-    // if (!match) return res.status(401).json({ message: 'Unauthorized' })
+    if (!match) return res.status(401).json({ message: 'Unauthorized' })
 
-    // const accessToken = jwt.sign(
-    //     {
-    //         "UserInfo": {
-    //             "username": foundUser.username,
-    //             "roles": foundUser.roles
-    //         }
-    //     },
-    //     process.env.ACCESS_TOKEN_SECRET,
-    //     { expiresIn: '2m' }
-    // )
+    const accessToken = jwt.sign(
+        {
+            "UserInfo": {
+                "username": foundUser.username,
+                "roles": foundUser.roles
+            }
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '2m' }
+    )
 
-    // const refreshToken = jwt.sign(
-    //     { "username": foundUser.username },
-    //     process.env.REFRESH_TOKEN_SECRET,
-    //     { expiresIn: '7d' }
-    // )
+    const refreshToken = jwt.sign(
+        { "username": foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '7d' }
+    )
 
-    // // Create secure cookie with refresh token 
-    // res.cookie('jwt', refreshToken, {
-    //     httpOnly: true, //accessible only by web server 
-    //     secure: true, //https
-    //     sameSite: 'None', //cross-site cookie 
-    //     maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
-    // })
+    // Create secure cookie with refresh token 
+    res.cookie('jwt', refreshToken, {
+        httpOnly: true, //accessible only by web server 
+        secure: true, //https
+        sameSite: 'None', //cross-site cookie 
+        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
+    })
 
-    // // Send accessToken containing username and roles 
-    // res.json({ accessToken })
+    // Send accessToken containing username and roles 
+    res.json({ accessToken })
 })
 // @desc Sign up
 // @route POST /auth/signup
 // @access Public
 const SignUp = asyncHandler(async (req, res) => {
 
-    // const { username, password } = req.body
+    const { username, password } = req.body
 
-    // // Confirm data
-    // if (!username || !password) {
-    //     return res.status(400).json({ message: 'username and password fields are required' })
-    // }
+    // Confirm data
+    if (!username || !password) {
+        return res.status(400).json({ message: 'username and password fields are required' })
+    }
 
-    // // Check for duplicate username
-    // const duplicate = await User.findOne({ username }).lean().exec()
+    // Check for duplicate username
+    const duplicate = await User.findOne({ username }).lean().exec()
 
-    // if (duplicate) {
-    //     return res.status(409).json({ message: 'Duplicate username' })
-    // }
+    if (duplicate) {
+        return res.status(409).json({ message: 'Duplicate username' })
+    }
 
-    // // Hash password 
-    // const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
+    // Hash password 
+    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
 
-    // var userObject = { username, "password": hashedPwd }
-    // const user = await User.create(userObject)
+    var userObject = { username, "password": hashedPwd }
+    const user = await User.create(userObject)
 
-    // if (user) { //created 
-    //     res.status(201).json({ message: `New user ${username} created` })
-    // } else {
-    //     res.status(400).json({ message: 'Invalid user data received' })
-    // }
+    if (user) { //created 
+        res.status(201).json({ message: `New user ${username} created` })
+    } else {
+        res.status(400).json({ message: 'Invalid user data received' })
+    }
 })
 // @desc Refresh
 // @route GET /auth/refresh
@@ -121,10 +121,10 @@ const refresh = (req, res) => {
 // @route POST /auth/logout
 // @access Public - just to clear cookie if exists
 const logout = (req, res) => {
-    // const cookies = req.cookies
-    // if (!cookies?.jwt) return res.sendStatus(204) //No content
-    // res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true })
-    // res.json({ message: 'Cookie cleared' })
+    const cookies = req.cookies
+    if (!cookies?.jwt) return res.sendStatus(204) //No content
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true })
+    res.json({ message: 'Cookie cleared' })
 }
 
 module.exports = {
